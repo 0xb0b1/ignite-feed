1bb634b5e0ec4849cbbf452a5994a97b0fcc6378
Paulo Vicente <vcente82@gmail.com>
2022-07-25 17:56:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+import "./styles/global.scss";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+export const Header: React.FC = () => {
+  return <header className={styles.header}>hello</header>;
+};
