44539da86a48b0568d8b9301c8ec41eff9d1deae
Paulo Vicente <vcente82@gmail.com>
2022-07-26 11:03:35 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 import "./styles/global.scss";
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <aside>Sidebar</aside>
+
+        <main>
+          <Post
+            author="Paulo Vicente"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+          <Post
+            author="Paulo Vicente"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        </main>
+      </div>
     </>
   );
 }
