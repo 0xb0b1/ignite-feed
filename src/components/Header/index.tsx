3900136a30b13ffbe1eb199cb30ddaf784e8ff20
Paulo Vicente <vcente82@gmail.com>
2022-07-26 10:27:05 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import igniteLogo from "../../assets/ignite-logo.svg";
 
 import styles from "./styles.module.scss";
 
 export const Header: React.FC = () => {
-  return <header className={styles.header}>hello</header>;
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Ignite Logo" />
+      <span>Ignite Feed</span>
+    </header>
+  );
 };
