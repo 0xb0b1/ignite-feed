4221edba5b74f52b990f101cb76835cbcbe85dcb
Paulo Vicente <vcente82@gmail.com>
2022-07-27 12:26:11 -0300

    post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles.module.scss";
+
 interface PostProps {
   author: string;
   content: string;
@@ -5,9 +7,28 @@ interface PostProps {
 
 export const Post = ({ author, content }: PostProps) => {
   return (
-    <div>
-      <h3>{author}</h3>
-      <span>{content}</span>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/0xb0b1.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>{author}</strong>
+            <span>Frontend Developer</span>
+          </div>
+        </div>
+
+        <time title="27 de Julho às 08:13h" dateTime="2022-07-27 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>{content}</p>
+      </div>
+    </article>
   );
 };
